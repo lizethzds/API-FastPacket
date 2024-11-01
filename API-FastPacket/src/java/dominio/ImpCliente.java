0b04736bc3531736ea27153859615fbbf6f0d504
Lizeth Zurutuza <111436787+lizethzds@users.noreply.github.com>
2024-10-31 23:09:48 -0600

    Registrar cliente
    Servicio terminado

--- API-FastPacket/src/java/dominio/ImpCliente.java
+++ API-FastPacket/src/java/dominio/ImpCliente.java
@@ -5,6 +5,8 @@ import java.util.List;
 import mybatis.MyBatisUtil;
 import org.apache.ibatis.session.SqlSession;
 import pojo.Cliente;
+import pojo.DatosRegistroCliente;
+import pojo.Mensaje;
 
 /**
  *
@@ -12,12 +14,59 @@ import pojo.Cliente;
  */
 public class ImpCliente {
     
-     public static List<Cliente> obtenerClientes(){
+     public static List<Cliente> obtenerClientes(){ 
         List<Cliente> clientes;
         SqlSession conexionBD = MyBatisUtil.getSession();
         clientes = conexionBD.selectList("clientes.obtenerLista");
         
         return clientes;
     }
+     
+     
+    public static DatosRegistroCliente obtenerClientePorId(Integer id)
+    {
+        DatosRegistroCliente clienteSolicitado;
+        SqlSession conexionBD = MyBatisUtil.getSession();
+        clienteSolicitado = conexionBD.selectOne("clientes.obtenerClientePorId",  id);
+     
+        return clienteSolicitado;
+        
+    }
+    
+    
+    public static Mensaje registrarCliente(DatosRegistroCliente registroCliente){
+       Mensaje msj = new Mensaje();
+       msj.setError(true);
+       SqlSession conexionBD = MyBatisUtil.getSession();
+       
+       if(conexionBD !=null){
+           try{
+               conexionBD.insert("clientes.registrar", registroCliente);
+               conexionBD.commit();
+               
+               if(registroCliente.getFilasAfectadas()>0 && registroCliente.getError().isEmpty()){
+                   msj.setError(false);
+                   msj.setContenido("Registro exitoso");
+               }else{
+                   msj.setContenido(registroCliente.getError());
+                   
+               }
+           }catch(Exception e){
+               msj.setContenido("Por el momento no se pudo realizar el registro, intente después.");
+               e.printStackTrace();
+           
+           }finally{
+               conexionBD.close();
+           }
+       }else{
+           msj.setContenido("Por el momento no hay conexión con la base de datos.");
+       }
+       
+       return msj;
+       
+   }
+   
+    
+    
     
 }
--- API-FastPacket/src/java/ws/WSCliente.java
+++ API-FastPacket/src/java/ws/WSCliente.java
@@ -5,13 +5,21 @@
  */
 package ws;
 
+import com.google.gson.Gson;
 import dominio.ImpCliente;
 import java.util.List;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import pojo.Cliente;
+import pojo.DatosRegistroCliente;
+import pojo.Direccion;
+import pojo.Mensaje;
 
 /**
  *
@@ -34,5 +42,33 @@ public class WSCliente {
     public List<Cliente> obtenerListaClientes(){
         return ImpCliente.obtenerClientes();
     }
+    
+    
+   @POST
+   @Path("registrar")
+   @Consumes(MediaType.APPLICATION_JSON)
+   @Produces(MediaType.APPLICATION_JSON)
+   
+   public Mensaje registrarCliente(String json){
+       Mensaje mensaje = new Mensaje();
+       
+       if(json.isEmpty()){
+           throw new WebApplicationException(Response.Status.BAD_REQUEST);
+       }else{
+           Gson gson = new Gson();
+           DatosRegistroCliente datosCliente = gson.fromJson(json, DatosRegistroCliente.class);
+           
+           Cliente cliente = datosCliente.getCliente();
+           Direccion direccion = datosCliente.getDireccion();
+           
+           if(cliente != null && direccion != null){
+               return ImpCliente.registrarCliente(datosCliente);
+               
+           }
+       }
+       
+       return mensaje;
+       
+   }
     
 }
